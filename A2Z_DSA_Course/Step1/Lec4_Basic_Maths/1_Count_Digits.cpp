1c3ac2e0abe2961d9873e8f9ce1acf334c001899
parameshwarank <44523413+parameshwarank@users.noreply.github.com>
2025-07-07 14:43:00 +0000

    Program Count Digits completed.
    Signed-off-by: parameshwarank <44523413+parameshwarank@users.noreply.github.com>

--- A2Z_DSA_Course/Step1/Lec4_Basic_Maths/1_Count_Digits.cpp
+++ A2Z_DSA_Course/Step1/Lec4_Basic_Maths/1_Count_Digits.cpp
@@ -1,9 +1,48 @@
+/*
+
+https://takeuforward.org/data-structure/count-digits-in-a-number/
+
+	Problem Statement: Given an integer N, return the number of digits in N.
+
+	Example 1:
+        Input:N = 12345
+               
+        Output:5
+              
+        Explanation:  The number 12345 has 5 digits.
+                                        
+        Example 2:
+        Input:N = 7789                
+                
+        Output: 4
+                
+        Explanation: The number 7789 has 4 digits.
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World" << endl;
+	int number, count = 0;
+
+	cout << "Enter a number:" << endl;
+	cin >> number;
+
+	while (number)
+	{
+		count++;
+		number = number / 10;
+	        cout << "Number : " << number << endl;
+	}
+
+	cout << "Total digits : " << count << endl;
 	return 0;
 }
+
+
+/*
+ * Tips - Divide by 10. It works on both positive and negative numbers.
+ */
+
